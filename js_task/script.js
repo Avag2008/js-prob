a73e28d48c67debaed8a709b4923e4adaf8787f2
Avag2008 <126093491+Avag2008@users.noreply.github.com>
2023-12-17 08:57:44 +0400

    Add files via upload

---
+++ js_task/script.js
@@ -0,0 +1,65 @@
+
+// ________________________________Task1_____________________________________
+// Version 1
+
+// let p = prompt("Enter your age in years")
+
+// function calcAge() {
+//     document.write(p * 365)
+// }   
+// calcAge()
+
+// Verson 2
+//  let p = prompt("Enter your age in years")
+
+//  document.write(p * 365)
+
+
+// _____________________________Task2________________________________________
+// let twoPoints = prompt("Enter how two points")
+// let threePoints = prompt("Enter how three points")
+// function basketball() {
+//     document.write(2 * twoPoints + 3 * threePoints);
+// }
+
+// basketball()
+// ____________________________Task3__________________________________________
+// let num1 = +prompt("Enter num1")
+// let num2 = +prompt("Enter num2")
+
+// if(num1 + num2 >= 100){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// ___________________________Task4___________________________________
+
+// let num = +prompt("Enter number");
+
+// function Factorial(num) {
+//   if ( num == 1) {
+//     return 1;
+//   } else {
+//     return num + Factorial(num - 1);
+//   }
+// }
+
+// console.log(Factorial(num));
+
+// _______________________Task5_____________________________________
+
+// let drinks = [
+//     { name: "lemonade", price: 50 },
+//     { name: "lime", price: 10 },
+//   ];
+  
+//   let sortDrinks = () => {
+//     if (drinks[0].price < drinks[1].price) {
+//       console.log(drinks[1], drinks[0]);
+//     } else {
+//       console.log(drinks[0], drinks[1]);
+//     }
+//   };
+  
+//   sortDrinks();
